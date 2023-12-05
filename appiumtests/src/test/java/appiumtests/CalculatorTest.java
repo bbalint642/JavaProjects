b80f4b6d96b62dc6ecb913d89d5598b81b176c52
Balázs Bálint Balázs <b.balint642@gmail.com>
2023-12-05 12:39:02 +0100

    Meetingen updatelt verzió

--- appiumtests/src/test/java/appiumtests/CalculatorTest.java
+++ appiumtests/src/test/java/appiumtests/CalculatorTest.java
@@ -1,21 +1,40 @@
 package appiumtests;
 
 import java.net.URL;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import io.appium.java_client.AppiumDriver;
-import org.openqa.selenium.WebElement;
-import io.appium.java_client.*;
 
-import org.openqa.selenium.WebDriver;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.android.AndroidDriver;
+
+//import org.openqa.selenium.WebElement;
+//import org.openqa.selenium.remote.RemoteWebDriver;
+//import io.appium.java_client.AppiumDriver;
+//import io.appium.java_client.MobileElement;
+//import org.openqa.selenium.WebElement;
+//import org.openqa.selenium.WebDriver;
 
 public class CalculatorTest {
 
 	//static WebDriver driver;
-	static AppiumDriver<MobileElement> driver;
-     
+	//static AppiumDriver<MobileElement> driver;
+    //WebElement driver;
+    
     
     public static void main(String[] args) {
         // TODO Auto-generated method stub
+    	
+    	try {
+    		openCalculator();
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println(e.getCause());
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+    	
+
     }
     
     public static void openCalculator() throws Exception {
@@ -26,12 +45,28 @@ public class CalculatorTest {
         cap.setCapability("platformName", "Android");
         cap.setCapability("platformVersion", "12");
         
-        cap.setCapability("appPackage", "com.google.android.calculator");
-        cap.setCapability("appActivity", "com.android.calculator2.Calculator");
+        cap.setCapability("appPackage", "com.telenor.mytelenor");
+        cap.setCapability("appActivity", "com.telenor.mytelenor.genesys.MainActivity");
         
-        URL url = new URL("http://127.0.0.1:4723/wd/hub");
-        dirver = new AppiumDriver<MobileElement>(cap);
-      
+        /*
+		cap.setCapability("appPackage", "com.google.android.calculator");
+        cap.setCapability("appActivity", "com.android.calculator2.Calculator");
+        */
+        
+        //URL url = new URL("http://127.0.0.1:4723/wd/hub");
+        
+        //RemoteWebDriver driver = new RemoteWebDriver(new URL("http://127.0.0.1:4723/wd/hub"), cap);
+        AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), cap);
+        
+        System.out.println("App elindult...");
+        
+        //driver = new RemoteWebDriver(url, cap);
+        
+        //AppiumDriver alma;
+        // Initialize the Appium Driver
+        //AppiumDriver<WebElement> driver = new AndroidDriver<>(new URL("http://0.0.0.0:4723/wd/hub"), caps);
+        //driver = new WebElement
+        //dirver = new AppiumDriver<MobileElement>(cap);
         //driver = new AppiumDriver(url, cap);
         
     }
